128a5a5a3ab333ddded6ed43a58e0438c1346746
devcarolinealmeida <carolineitalia85@gmail.com>
2024-12-03 13:09:27 +0100

    ADD: section perche seguirci

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { ChiSiamo } from "../sections/ChiSiamo/page";
 import Header from "../sections/Header/page";
 import { Hero } from "../sections/Hero/page";
+import { PercheSeguirci } from "../sections/PercheSeguirci/page";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Header/>
       <Hero/>
       <ChiSiamo/>
+      <PercheSeguirci/>
     </>
   );
 }
---
+++ sections/PercheSeguirci/page.tsx
@@ -0,0 +1,19 @@
+export const PercheSeguirci = () => {
+  return (
+    <section className="bg-[#FCE688] py-16" id="perche-seguirci">
+      <div className="container max-w-7xl flex flex-col md:flex-row">
+        <div className="">
+          <h4 className="mb-4 text-4xl">Perchè Seguirci</h4>
+          <p className="">
+            Semplice. Ti diamo consigli su come poter vivere e mangiare
+            spendendo pochissimo! Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
