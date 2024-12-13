0ed63107c1fa7789a76a64f08971d19c5104e034
devcarolinealmeida <carolineitalia85@gmail.com>
2024-12-13 19:09:51 +0100

    UPDATE: text-align hero

--- sections/Hero/page.tsx
+++ sections/Hero/page.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
   return (
     <section className="_hero pt-4 md:pt-12 pb-24 flex flex-col bg-[#B1ABF4] overflow-x-clip" id="top">
       <div className="container max-w-7xl">
-        <div className="flex flex-col items-center relative">
+        <div className="flex flex-col items-start md:items-center relative">
           <motion.h2 
           initial={{
             translateY: '100%',
@@ -23,7 +23,7 @@ export const Hero = () => {
             visualDuration: 0.5,
             bounce: .75
           }}
-          className="text-center md:max-w-[24ch] mb-8">Vida, Viagens, Vlog</motion.h2>
+          className="md:text-center md:max-w-[24ch] mb-8">Vida, Viagens, Vlog</motion.h2>
           <motion.div
           initial={{
             transform: 'rotateZ(-35deg)',
@@ -47,7 +47,7 @@ export const Hero = () => {
           className="_tag-hello absolute top-[25%] md:top-[50%] right-0">
             <TagHello className="w-[7.5rem] h-[6.5rem]"/>
           </motion.div>
-          <p className="text-xl lg:text-2xl text-center max-w-[24ch] md:max-w-[42ch] mb-8">
+          <p className="text-xl lg:text-2xl md:text-center max-w-[24ch] md:max-w-[42ch] mb-8">
             Dois brasileiros aprendendo a viver na Itália
           </p>
           <Link className="btn-pulse" href="#chi_siamo" role="link" aria-label="Clique e descubra mais">
