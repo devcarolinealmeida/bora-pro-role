7a60cd876784173f8919561592f0e7fc9d2fc620
devcarolinealmeida <carolineitalia85@gmail.com>
2024-12-20 17:02:28 +0100

    FIX: min-h menu mob

--- sections/Header/index.tsx
+++ sections/Header/index.tsx
@@ -46,7 +46,7 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <div className="_nav-mob bg-[#B1ABF4] flex md:hidden justify-between items-center p-4">
+        <div className="_nav-mob bg-[#B1ABF4] flex md:hidden justify-between items-center p-4 min-h-[4.5rem]">
           <Link href="#top">
             <h1 className="text-[1.375rem] font-asap-condens-900">
               Bora pro rolê
